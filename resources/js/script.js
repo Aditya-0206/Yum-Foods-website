5466951ea39455dddad6afc07d94fb2617d19e16
Aditya-0206 <adithyarampure619@gmail.com>
2021-06-08 09:50:18 +0530

    Final commit as of 31/05/2021

---
+++ resources/js/script.js
@@ -0,0 +1,99 @@
+$(document).ready(function(){
+    /* Sticky navgation */
+
+    $('.js--section-features').waypoint(function(direction){
+        if(direction == "down"){
+            $('nav').addClass('sticky');
+        }
+        else{
+            $('nav').removeClass('sticky');
+        }
+    },{
+        offset:'60px;'
+    });
+
+    /* Button Scroll*/
+
+    $('.js--button-plans').click(function (){
+        $('html,body').animate({scrollTop: $('.js--section-plans').offset().top}, 1500);
+    });
+    $('.js--button-features').click(function (){
+        $('html,body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+    /* navigation scrolling */
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+    // On-page links
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+    ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+            } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+            };
+        });
+        }
+    }
+    });
+
+    /* animations */
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animate__animated animate__zoomIn');
+    },{
+        offset: '50%'    
+    });
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+    },{
+        offset: '50%'    
+    });
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    },{
+        offset: '50%'    
+    });
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
+    },{
+        offset: '50%'    
+    });
+
+    // nav icon change
+    $('.js--nav-icon ').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        if(icon.hasClass('fa-bars')){
+            icon.addClass('fa-times');
+            icon.removeClass('fa-bars');
+        }
+        else{
+            icon.addClass('fa-bars');
+            icon.removeClass('fa-times');
+        }
+    });
+});
